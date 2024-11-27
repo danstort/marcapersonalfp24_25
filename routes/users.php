38b7bbf28e841aaf8d3753f9bd8658a8266f85d1
Daniel Soto Ortiz <daniel.soto.ortiz@gmail.com>
2024-11-27 08:17:16 +0100

    Agrupando rutas con prefijos

--- routes/users.php
+++ routes/users.php
@@ -2,7 +2,10 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('users', [UserController::class, 'getIndex']);
-Route::get('users/show/{id}', [UserController::class, 'getShow'])->where('id', '[0-9]*');
-Route::get('users/create', [UserController::class, 'getCreate']);
-Route::get('users/edit/{id}', [UserController::class, 'getEdit'])->where('id', '[0-9]*');
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::get('users', [UserController::class, 'getIndex']);
+    Route::get('users/show/{id}', [UserController::class, 'getShow'])->where('id', '[0-9]*');
+    Route::get('users/create', [UserController::class, 'getCreate']);
+    Route::get('users/edit/{id}', [UserController::class, 'getEdit'])->where('id', '[0-9]*');
+
+});
